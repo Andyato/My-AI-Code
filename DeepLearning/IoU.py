901c19972b0b58244010beab2c178228f53f32e7
Andyato <andyato@163.com>
2019-07-28 15:26:12 +0800

    update Iou

--- DeepLearning/IoU.py
+++ DeepLearning/IoU.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def get_IoU(pred_bbox, gt_bbox):
-    '''@pred_bbox & gt_bbox : [xmin, ymin, xmax, ymax]
+    '''@pred_bbox & gt_bbox : np.ndarray [xmin, ymin, xmax, ymax]
     '''
     assert pred_bbox[2] - pred_bbox[0] > 0 and pred_bbox[3] - pred_bbox[1] > 0
     assert gt_bbox[2] - gt_bbox[0] > 0 and gt_bbox[3] - gt_bbox[1] > 0
@@ -13,23 +13,21 @@ def get_IoU(pred_bbox, gt_bbox):
     iymax = min(pred_bbox[3], gt_bbox[3])
     iw = np.maximum(ixmax - ixmin + 1., 0.)
     ih = np.maximum(iymax - iymin + 1., 0.)
-    # iw = max(ixmax - ixmin + 1., 0.)
-    # ih = max(iymax - iymin + 1., 0.)
 
     # intersection
-    inters = iw * ih
+    inter = iw * ih
 
-    # union, union = S1 + S2 - inters
+    # union, union = S1 + S2 - inter
     S1 = (pred_bbox[2] - pred_bbox[0] + 1.) * (pred_bbox[3] - pred_bbox[1] + 1.)
     S2 = (gt_bbox[2] - gt_bbox[0] + 1.) * (gt_bbox[3] - gt_bbox[1] + 1.)
-    union = S1 + S2 - inters
+    union = S1 + S2 - inter
 
-    # overlaps
-    overlaps = inters / union
-    return overlaps
+    # overlap
+    overlap = inter / union
+    return overlap
 
 
 if __name__ == "__main__":
-    pred_bbox = [0, 0, 10, 10]
-    gt_bbox = [0, 0, 10, 10]
+    pred_bbox = np.array([0, 0, 10, 10])
+    gt_bbox = np.array([0, 0, 10, 10])
     print(get_IoU(pred_bbox, gt_bbox))
